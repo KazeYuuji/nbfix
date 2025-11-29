8ae6ed7e3ba9f39c424ff905b72899ab6efe16ff
KazeYuuji <zinhalcrafter@gmail.com>
2025-11-29 18:08:45 +0700

    wine

--- train_model.py
+++ train_model.py
@@ -10,7 +10,7 @@ df = pd.read_csv('WineQT.csv')
 X = df.drop(['quality', 'Id'], axis=1)
 y = df['quality']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 nb_model = GaussianNB()
 nb_model.fit(X_train, y_train)
